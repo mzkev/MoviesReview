e7cce152771b2deef217c0a6730fff1fc9ed4027
Ochuko Kevwe <kevweochu@gmail.com>
2019-07-04 00:29:00 +0100

    Initial commit

---
+++ Model/Index.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesReviewApp.Model
+{
+    public class Index
+    {
+        public int id { get; set; }
+        [Display(Name = "Rating")]
+        public double vote_average { get; set; }
+        public string title { get; set; }
+        [Display(Name = "Total Votes")]
+        public int vote_count { get; set; }
+        public byte[] poster_path { get; set; }
+        public string overview { get; set; }
+        [Display(Name ="Release Date")]
+        public DateTime release_date { get; set; }
+        [Display(Name = "Genre")]
+        public string genre { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesReviewApp.Model;
+using Newtonsoft.Json;
+
+namespace MoviesReviewApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+       public IList<Index> Index { get; set; }
+        HttpClient httpClient = new HttpClient();
+        
+        public async Task OnGet()
+        {
+            var response = await httpClient.GetAsync("https://api.themoviedb.org/3/movie/popular?api_key=929321ac36adb5b4c6cf9d99173ce6fb&language=en-US&page=1");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(apiResponse);
+                Index = JsonConvert.DeserializeObject<IList<Index>>(apiResponse).ToList();
+
+            }
+        }
+    }
+}
